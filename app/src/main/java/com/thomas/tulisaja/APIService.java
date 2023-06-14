f872c1ad5e8c80c67940bb3757a959b7a64941e9
Thomas2125250040 <113816448+Thomas2125250040@users.noreply.github.com>
2023-06-14 09:06:04 +0700

    initial commit

---
+++ app/src/main/java/com/thomas/tulisaja/APIService.java
@@ -0,0 +1,44 @@
+//02
+package com.thomas.tulisaja;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @FormUrlEncoded
+    @POST("getAllPost")
+    Call<ValueData<List<Post>>> getPost(@Field("key") String key);
+
+    @FormUrlEncoded
+    @POST("loginUser")
+    Call<ValueNoData> login(@Field("key") String key,
+                            @Field("username")String username,
+                            @Field("password")String password);
+    @FormUrlEncoded
+    @POST("RegisterUser")
+    Call<ValueNoData> register(@Field("key") String key,
+                            @Field("username")String username,
+                            @Field("password")String password);
+
+    @FormUrlEncoded
+    @POST("insertPost")
+    Call<ValueNoData> addPost(@Field("key") String key,
+                               @Field("username")String username,
+                               @Field("content")String content);
+    @FormUrlEncoded
+    @POST("updatePost")
+    Call<ValueNoData> updatePost(@Field("key") String key,
+                                 @Field("id")String id,
+                                 @Field("content")String content);
+
+    @FormUrlEncoded
+    @POST("deletePost")
+    Call<ValueNoData> deletePost(@Field("key") String key,
+                                 @Field("id")String id);
+
+
+}
